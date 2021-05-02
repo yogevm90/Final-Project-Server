534b30c7d8675d6a99c044560799b4f732121e56
Idan Cohen <idan57@gmail.com>
2021-05-02 20:46:18 +0300

    change user creation to call bash SCRIPT instead
    Signed-off-by: Idan Cohen <idan57@gmail.com>

--- server/stream_manager/stream_manager.py
+++ server/stream_manager/stream_manager.py
@@ -5,13 +5,13 @@ from server.stream_manager.exceptions import StreamUserException
 
 def add_user(username, password):
     current_dir = os.path.dirname(__file__)
-    return_code = os.system('sudo {}/scripts/add_user.sh {} {}'.format(current_dir, username, password))
+    return_code = os.system('bash {}/scripts/add_user.sh {} {}'.format(current_dir, username, password))
     if return_code != 0:
         raise StreamUserException('Failed to add user {} to stream server'.format(username))
 
 
 def change_password(username, password):
     current_dir = os.path.dirname(__file__)
-    return_code = os.system('sudo {}/scripts/change_password.sh {} {}'.format(current_dir, username, password))
+    return_code = os.system('bash {}/scripts/change_password.sh {} {}'.format(current_dir, username, password))
     if return_code != 0:
         raise StreamUserException('failed to change password to user {} on stream server'.format(username))
