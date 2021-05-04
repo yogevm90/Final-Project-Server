79e1ad84b5f96bfad7dfc034f57ad745551f7a8d
Idan Cohen <idan57@gmail.com>
2021-05-04 23:36:46 +0300

    add video streamer
    Signed-off-by: Idan Cohen <idan57@gmail.com>

---
+++ flask_microservices/video_microservice/video_app.py
@@ -0,0 +1,69 @@
+import os
+from pathlib import Path
+from threading import Lock
+
+import flask
+from flask_cors import CORS
+
+from flask_microservices.flask_executor.flask_app_base import FlaskAppBase
+from utilities.logging.scholapp_server_logger import ScholappLogger
+
+
+class ImagesContainer(object):
+    def __init__(self):
+        self._images = {}
+
+    def get_image(self, user):
+        if user in self._images:
+            self._images[user]["lock"].acquire()
+            image = self._images[user]["image"]
+            self._images[user]["lock"].release()
+            return image
+
+    def add_image(self, image, user):
+        if user not in self._images:
+            self._images[user] = {
+                "image": bytearray(),
+                "lock": Lock()
+            }
+        self._images[user]["lock"].acquire()
+        self._images[user]["image"] = image
+        self._images[user]["lock"].release()
+
+
+class VideoApp(FlaskAppBase):
+    """
+    A class for a microservice to save images
+    """
+
+    def __init__(self, import_name="VideoApp", **kwargs):
+        """
+        :param import_name: import name
+        :param kwargs: any dict arguments needed
+        """
+        super().__init__(import_name, **kwargs)
+        super()._chdir(__file__)
+        self._img_counter = 1
+        ScholappLogger.info(f"Setting up {import_name}")
+        CORS(self, resources={r"/GetImage": {"origins": "*"}})
+        self._images = ImagesContainer()
+        self._setup()
+        ScholappLogger.info(f"Setting up was successful")
+
+    def _setup(self):
+        """
+        Setup REST API routes
+        """
+
+        @self.route("/GetImage/<user>")
+        def get_img(user):
+            image = self._images.get_image(user)
+            if image:
+                return flask.Response(self._images.get_image(user), mimetype="image/jpg")
+            else:
+                return flask.redirect("/static/default.jpg")
+
+        @self.route("/UploadImage/<user>", methods=["POST"])
+        def upload_img(user):
+            self._images.add_image(flask.request.data, user)
+            return flask.jsonify({"verdict": True})
