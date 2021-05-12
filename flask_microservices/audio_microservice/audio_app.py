50fece6c9791c4877ff0852e9a78b18d214512d9
Idan Cohen <idan57@gmail.com>
2021-05-12 21:57:57 +0300

    add audio path removal
    Signed-off-by: Idan Cohen <idan57@gmail.com>

--- flask_microservices/audio_microservice/audio_app.py
+++ flask_microservices/audio_microservice/audio_app.py
@@ -1,12 +1,10 @@
-import io
-import copy
 import os
-from multiprocessing import Lock
+import shutil
 from pathlib import Path
 
 import flask
 from flask_compress import Compress
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 from flask_microservices.flask_executor.flask_app_base import FlaskAppBase
 from utilities.logging.scholapp_server_logger import ScholappLogger
@@ -30,6 +28,8 @@ class AudioApp(FlaskAppBase):
         self._compress = Compress()
         self._compress.init_app(self)
         self._static_folder = Path(os.path.dirname(__file__)) / "static"
+        if not self._static_folder.is_dir():
+            self._static_folder.mkdir()
         self._setup()
         ScholappLogger.info(f"Setting up was successful")
 
@@ -38,12 +38,32 @@ class AudioApp(FlaskAppBase):
         Setup REST API routes
         """
 
+        @self.route("/DeleteAudioPath", methods=["POST"])
+        @self._compress.compressed()
+        def get_audio_path():
+            login_details = flask.request.get_json()
+            class_id = login_details["class_id"]
+            username = login_details["username"]
+            if class_id in self._audios and username in self._audios[class_id]:
+                if self._audios[class_id][username].is_dir():
+                    shutil.rmtree(str(self._audios[class_id][username]))
+
         @self.route("/GetAudioPath", methods=["POST"])
         @self._compress.compressed()
         def get_audio_path():
             login_details = flask.request.get_json()
-            user_p = self._static_folder / login_details["username"]
+            class_id = login_details["class_id"]
+            username = login_details["username"]
+
+            class_p = self._static_folder / class_id
+            if not class_p.is_dir():
+                class_p.mkdir()
+
+            user_p = class_p / username
             if not user_p.is_dir():
                 user_p.mkdir()
-            self._audios[login_details["username"]] = user_p
+
+            if class_id not in self._audios:
+                self._audios[class_id] = {}
+            self._audios[class_id][username] = user_p
             return flask.Response(str(user_p))
