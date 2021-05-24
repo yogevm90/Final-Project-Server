cf1d38cec611f4f39f55a050af5a7e5b4a2396f7
yogevm90 <yogevm90@gmail.com>
2021-05-24 18:23:13 +0300

    Init server update

--- init_server.py
+++ init_server.py
@@ -11,8 +11,10 @@ classes_col = mydb["classes"]
 users_col.remove()
 classes_col.remove()
 
-class1_query = {'class_name': 'math', 'class_id': '00003', 'teacher': 'admin', 'description': 'math class', 'external_links': ['www.google.com', 'www.facebook.com'], 'homework_links': ['www.ynet.co.il'], 'updates': ['update 1', 'update 2'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 2, 'start': '2021-05-10T10:00:00', 'end': '2021-05-10T11:00:00', 'img_path': 'www.walla.co.il'}
-
+class1_query = {'class_name': 'math', 'class_id': '00003', 'teacher': 'admin', 'description': 'math class', 'external_links': ['www.google.com', 'www.facebook.com'], 'homework_links': ['www.ynet.co.il'], 'updates': ['update 1', 'update 2'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 2, 'start': '2021-05-10T10:00:00', 'end': '2021-05-10T11:00:00', 'img_path': 'https://i.imgur.com/wy3M2bY.jpeg'}
+class2_query = {'class_name': 'physics', 'class_id': '00004', 'teacher': 'admin', 'description': 'its a nice good class', 'external_links': ['www.wiki.com', 'www.facebook.com'], 'homework_links': ['www.ynet.co.il'], 'updates': ['update 1', 'update 2'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 4, 'start': '2021-05-12T16:00:00', 'end': '2021-05-12T18:00:00', 'img_path': 'https://i.imgur.com/UJ6LIOh.jpeg'}
+class3_query = {'class_name': 'google', 'class_id': '00005', 'teacher': 'admin', 'description': 'google searching all day long', 'external_links': ['www.google.com', 'www.facebook.com'], 'homework_links': ['www.ynet.co.il'], 'updates': ['update 1', 'update 2'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 4, 'start': '2021-05-12T17:00:00', 'end': '2021-05-12T19:00:00', 'img_path': 'https://i.imgur.com/46DYgv1.jpeg'}
+class4_query = {'class_name': 'tanah', 'class_id': '00006', 'teacher': 'admin', 'description': 'bible class', 'external_links': ['www.google.com', 'www.facebook.com'], 'homework_links': ['www.ynet.co.il'], 'updates': ['update 1', 'update 2'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 3, 'start': '2021-05-11T10:00:00', 'end': '2021-05-11T11:00:00', 'img_path': 'https://i.imgur.com/4ixmwP1.jpeg'}
 yogev_pass = "yogev"
 idan_pass = "idan"
 admin_pass = "admin"
