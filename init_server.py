6bda38f8eaf2e3e39ef36330d918d0de3e7b86f1
yogevm90 <yogevm90@gmail.com>
2021-05-31 20:04:20 +0300

    Update init script for db

--- init_server.py
+++ init_server.py
@@ -11,24 +11,18 @@ classes_col = mydb["classes"]
 users_col.remove()
 classes_col.remove()
 
-class1_query = {'class_name': 'math', 'class_id': '00003', 'teacher': 'admin', 'description': 'math class', 'external_links': ['www.google.com', 'www.facebook.com'], 'homework_links': ['www.ynet.co.il'], 'updates': ['update 1', 'update 2'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 2, 'start': '2021-05-10T10:00:00', 'end': '2021-05-10T11:00:00', 'img_path': 'https://i.imgur.com/wy3M2bY.jpeg'}
-class2_query = {'class_name': 'physics', 'class_id': '00004', 'teacher': 'admin', 'description': 'its a nice good class', 'external_links': ['www.wiki.com', 'www.facebook.com'], 'homework_links': ['www.ynet.co.il'], 'updates': ['update 1', 'update 2'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 4, 'start': '2021-05-12T16:00:00', 'end': '2021-05-12T18:00:00', 'img_path': 'https://i.imgur.com/UJ6LIOh.jpeg'}
-class3_query = {'class_name': 'google', 'class_id': '00005', 'teacher': 'admin', 'description': 'google searching all day long', 'external_links': ['www.google.com', 'www.facebook.com'], 'homework_links': ['www.ynet.co.il'], 'updates': ['update 1', 'update 2'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 4, 'start': '2021-05-12T17:00:00', 'end': '2021-05-12T19:00:00', 'img_path': 'https://i.imgur.com/46DYgv1.jpeg'}
-class4_query = {'class_name': 'tanah', 'class_id': '00006', 'teacher': 'admin', 'description': 'bible class', 'external_links': ['www.google.com', 'www.facebook.com'], 'homework_links': ['www.ynet.co.il'], 'updates': ['update 1', 'update 2'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 3, 'start': '2021-05-11T10:00:00', 'end': '2021-05-11T11:00:00', 'img_path': 'https://i.imgur.com/4ixmwP1.jpeg'}
+class1_query = {'class_name': 'Computer Architecture', 'class_id': '89230', 'teacher': 'idan57', 'description': 'Development of systemic thinking, significant improvement in students abilities in systems development, and radical change in their understanding of the work of the modern computer', 'external_links': ['https://cs.biu.ac.il/staff/121', 'https://shoham.biu.ac.il/BiuCoursesViewer/CourseSylabusView.aspx?lid=720146'], 'homework_links': [], 'updates': ['No homework for the entire semester', 'Jokes'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 2, 'start': '2021-05-10T10:00:00', 'end': '2021-05-10T11:30:00', 'img_path': 'https://cs.biu.ac.il/sites/default/files/styles/4_cols_square_img/public/2019-12/%D7%A4%D7%A8%D7%95%D7%A4%27%20%D7%92%D7%9C%20%D7%A7%D7%9E%D7%99%D7%A0%D7%A7%D7%90.jpg?itok=vLgshuw_'}
+class2_query = {'class_name': 'Algorithems 2', 'class_id': '89322', 'teacher': 'idan57', 'description': 'More advanced problems in methods studied in Algorithms I. New algorithmic methods, e.g. Linear programming. Different algorithmic paradigms, e.g. on-line algorithms, approximation algorithms, streaming, distributed algorithms, and parallel algorithms. New topics, e.g. pattern matching, compression', 'external_links': ['https://cs.biu.ac.il/staff/103', 'https://u.cs.biu.ac.il/~amir/', 'https://shoham.biu.ac.il/BiuCoursesViewer/CourseSylabusView.aspx?lid=720186'], 'homework_links': ['https://drive.google.com/drive/folders/0B_dY_D5Av8zpY19UenV3bTdNejA'], 'updates': ['Whoever approves that NP=P get 5 more points to the final score'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 4, 'start': '2021-05-12T16:00:00', 'end': '2021-05-12T18:00:00', 'img_path': 'https://cs.biu.ac.il/sites/default/files/styles/4_cols_square_img/public/2019-12/%D7%90%D7%9E%D7%99%D7%A8%20%D7%A2%D7%9E%D7%99%D7%94%D7%95%D7%93.jpg?itok=s36nN1Xw'}
+class3_query = {'class_name': 'Introduction to Computer Science', 'class_id': '89110', 'teacher': 'idan57', 'description': 'Computer science Introduction. Programming in C. Basic concepts in algorithms. Basic hardware optimizations', 'external_links': ['https://cs.biu.ac.il/staff/62', 'https://u.cs.biu.ac.il/~agmon/'], 'homework_links': ['https://lemida.biu.ac.il/'], 'updates': ['Final test cancelled'], 'test_ids': [], 'participants': ['yogevm90', 'idan57'], 'day': 4, 'start': '2021-05-12T17:00:00', 'end': '2021-05-12T19:00:00', 'img_path': 'https://cs.biu.ac.il/sites/default/files/styles/4_cols_square_img/public/2019-05/noaagmon.jpg?itok=vbr0cw5s'}
 yogev_pass = "yogev"
 idan_pass = "idan"
-admin_pass = "admin"
 password1 = bcrypt.hashpw(yogev_pass.encode(), bcrypt.gensalt())
 password2 = bcrypt.hashpw(idan_pass.encode(), bcrypt.gensalt())
-password3 = bcrypt.hashpw(admin_pass.encode(), bcrypt.gensalt())
-user1_query = {'username': 'yogevm90', 'password': password1, 'id': '00001', 'name': 'Yogev', 'surname': 'Mela', 'role': 'student', 'classes': ['00003', '00004']}
-user2_query = {'username': 'idan57', 'password': password2, 'id': '00002', 'name': 'Idan', 'surname': 'Cohen', 'role': 'teacher', 'classes': ['00003', '00005', '00004']}
-admin_query = {'username': 'admin', 'password': password3, 'id': '00000', 'name': 'admin', 'surname': 'admin', 'role': 'teacher', 'classes': []}
+user1_query = {'username': 'yogevm90', 'password': password1, 'id': '00001', 'name': 'Yogev', 'surname': 'Mela', 'role': 'student', 'classes': ['89230', '89322']}
+user2_query = {'username': 'idan57', 'password': password2, 'id': '00002', 'name': 'Idan', 'surname': 'Cohen', 'role': 'teacher', 'classes': ['89230', '89110', '89322']}
 
 classes_col.insert_one(class1_query)
 classes_col.insert_one(class2_query)
 classes_col.insert_one(class3_query)
-classes_col.insert_one(class4_query)
 users_col.insert_one(user1_query)
 users_col.insert_one(user2_query)
-users_col.insert_one(admin_query)
